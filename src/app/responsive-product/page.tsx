f57bb67dd2ea979e740570bdb6b6c4839951680a
User <user@192-168-1-104.tpgi.com.au>
2024-07-01 15:53:29 +1000

    ok

--- src/app/responsive-product/page.tsx
+++ src/app/responsive-product/page.tsx
@@ -4,9 +4,9 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 export default function Home() {
     return (
         <div className="flex h-screen justify-center items-center bg-[#f2ebe5]">
-            <div className="w-[320px] bg-white rounded-2xl font-montserrat text-[#7d7d85] text-[14px] flex flex-col lg:flex-row lg:w-auto">
+            <div className="w-[320px] bg-white rounded-2xl font-montserrat text-[#7d7d85] text-[14px] flex flex-col lg:flex-row lg:w-[600px]">
                 <div className="w-[320px] h-[200px] bg-cover rounded-t-2xl bg-[url('/responsive-product/product-mobile.jpg')] lg:bg-[url('/responsive-product/product-desktop.jpg')] lg:h-auto lg:w-[200px] lg:rounded-tr-none lg:rounded-l-2xl"></div>
-                <div className="p-6 ">
+                <div className="p-6 lg:w-[400px]">
                     <div className="mb-2">P E R F U M E</div>
                     <div className="font-fraunces text-[26px] leading-8 mb-3 text-[#1e2629]">Gabrielle Essence Eau De Parfum</div>
                     <div className="mb-3">A floral, solar and voluptuous interpretation composed by oliver Polge, Perfumer-creator for the House of CHANEL.</div> 
